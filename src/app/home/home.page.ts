802a4f0ea83e04d3364dad646faf6faa1b85f300
Manuel <maneduron91@gmail.com>
2020-07-12 20:37:45 -0700

    pruebas con capacitor storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const {Storage} = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -19,6 +22,41 @@ export class HomePage implements OnInit{
   Submit(){
     console.log('si se enviaron los datos');
     console.log(this.datos);
+  }
+
+
+  async setItem() {
+    const cartvalue = JSON.stringify([{
+      id: 1,
+      product: this.datos.dato1
+    }, {
+      id: 2,
+      product: this.datos.dato2
+    }]);
+
+    await Storage.set({
+      key: 'products',
+      value: cartvalue
+    });
+    await Storage.set({
+      key: 'user',
+      value: 'Usuario'
+    });
+  }
+
+  async getItem() {   // obtener valor de los datos en consola
+    const products = await Storage.get({key: 'products'});
+    console.log(JSON.parse(products.value));
+  }
+  async removeItem() {  // remover un valor de los datos
+    await Storage.remove({ key: 'products'});
+  }
+  async getKeys() {
+    const keys = await Storage.keys();
+    console.log('Keys ', keys);
+  }
+  async clearStorage() {  // limpiar datos
+    await Storage.clear();
   }
 
 }
