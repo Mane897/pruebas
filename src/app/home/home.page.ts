7270f77ead6a0ecf81a1f1c4da28baccb514b648
Manuel <maneduron91@gmail.com>
2020-07-12 17:38:02 -0700

    Input para recibir datos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  datos = {
+    dato1: '',
+    dato2: ''
+  };
 
   constructor() {}
+
+  ngOnInit(){
+  }
+  Submit(){
+    console.log('si se enviaron los datos');
+    console.log(this.datos);
+  }
 
 }
